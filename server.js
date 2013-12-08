fcb93d09776ae739d9d2a2ff53131af7ea67b4ba
roosens_paul <roosens.p@gmail.com>
2013-12-08 17:31:09 +0100

    for server

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.configure(function () {
 var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1" ;
 var port =   process.env.OPENSHIFT_NODEJS_PORT || 80 ;
 
+
+
   server.listen(port  , ipaddr) ;
 
 // routing
